3720f02f3f1609cec6b22f627232903c640ec3e5
patrickGomulka7564 <n01347564@humbermail.ca>
2021-09-27 02:05:39 -0400

    Test commit.

--- SmartPlant/src/main/java/ca/greenlypebble/it/smartplant/ui/home/HomeViewModel.java
+++ SmartPlant/src/main/java/ca/greenlypebble/it/smartplant/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("PAGE #2");
     }
 
     public LiveData<String> getText() {
