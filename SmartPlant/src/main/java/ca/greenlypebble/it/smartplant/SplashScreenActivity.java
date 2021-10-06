e0ef8020cfae75c7fcb2f7dee7fd8baab199f32a
riccigamiao3076 <n01363076@humbermail.ca>
2021-10-06 12:53:46 -0400

    Splash Screen for the app created.

---
+++ SmartPlant/src/main/java/ca/greenlypebble/it/smartplant/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package ca.greenlypebble.it.smartplant;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+        finish();
+    }
+}
