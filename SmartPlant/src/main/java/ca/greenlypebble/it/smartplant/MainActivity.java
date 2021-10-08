2d36f30932752f915ea781c5de4b881eb18f5e03
patrickGomulka7564 <n01347564@humbermail.ca>
2021-10-07 20:20:19 -0400

    Removed navigation menu from login page.

--- SmartPlant/src/main/java/ca/greenlypebble/it/smartplant/MainActivity.java
+++ SmartPlant/src/main/java/ca/greenlypebble/it/smartplant/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        // NavigationUI.setupWithNavController(navView, navController);
     }
 
     @Override
