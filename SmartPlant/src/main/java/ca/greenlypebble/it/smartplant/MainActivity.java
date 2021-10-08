83b0607a3b888f872f24fe653b2e157bfc745384
riccigamiao3076 <n01363076@humbermail.ca>
2021-10-07 22:23:27 -0400

    Sign in page updated with temporary validation. Enter "admin" for email and pass to sign in.

--- SmartPlant/src/main/java/ca/greenlypebble/it/smartplant/MainActivity.java
+++ SmartPlant/src/main/java/ca/greenlypebble/it/smartplant/MainActivity.java
@@ -5,7 +5,11 @@
 package ca.greenlypebble.it.smartplant;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -16,7 +20,13 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import ca.greenlypebble.it.smartplant.ui.dashboard.DashboardFragment;
+import ca.greenlypebble.it.smartplant.ui.home.Page1;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button signIn;
+    EditText emailAdd, passWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +40,29 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        // NavigationUI.setupWithNavController(navView, navController);
+
+        signIn = (Button) findViewById(R.id.signInbutton);
+        emailAdd = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        passWord = (EditText) findViewById(R.id.editTextTextPassword);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String
+                        checkEA = emailAdd.getText().toString(),
+                        checkPW = passWord.getText().toString();
+
+                if (checkEA.matches("admin") && checkPW.matches("admin") )
+                    setContentView(R.layout.fragment_page2);
+
+                else {
+
+                }
+                // NavigationUI.setupWithNavController(navView, navController);
+            }
+        });
     }
+
 
     @Override
     public void onBackPressed() {
