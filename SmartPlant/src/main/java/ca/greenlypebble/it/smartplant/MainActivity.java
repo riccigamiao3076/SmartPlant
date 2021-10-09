df07125d095c62331d137fca4b9f4191dfa32fd3
riccigamiao3076 <n01363076@humbermail.ca>
2021-10-09 15:20:45 -0400

    Main Activity and activity_main.xml updated.

--- SmartPlant/src/main/java/ca/greenlypebble/it/smartplant/MainActivity.java
+++ SmartPlant/src/main/java/ca/greenlypebble/it/smartplant/MainActivity.java
@@ -26,22 +26,19 @@ import ca.greenlypebble.it.smartplant.ui.home.Page1;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button signIn;
-    EditText emailAdd, passWord;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // BottomNavigationView navView = findViewById(R.id.nav_view);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        // NavigationUI.setupWithNavController(navView, navController)
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 
 
