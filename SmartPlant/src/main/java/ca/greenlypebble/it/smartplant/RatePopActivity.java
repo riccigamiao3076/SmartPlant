ef57125fdf6e4cdab712ee7e4fbd8af79c7db189
riccigamiao3076 <n01363076@humbermail.ca>
2021-11-13 14:27:02 -0500

    New Activity for the "Rate the App" button.

---
+++ SmartPlant/src/main/java/ca/greenlypebble/it/smartplant/RatePopActivity.java
@@ -0,0 +1,15 @@
+package ca.greenlypebble.it.smartplant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RatePopActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate_pop);
+    }
+}
