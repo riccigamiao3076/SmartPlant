ba47cd70f80ee773f38641150ad660bf2b2e8df0
riccigamiao3076 <n01363076@humbermail.ca>
2021-11-13 15:01:19 -0500

    Added Pop Up Window for Rate App button.

--- SmartPlant/src/main/java/ca/greenlypebble/it/smartplant/RatePopActivity.java
+++ SmartPlant/src/main/java/ca/greenlypebble/it/smartplant/RatePopActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.WindowManager;
 
 public class RatePopActivity extends Activity {
 
@@ -11,5 +14,20 @@ public class RatePopActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate_pop);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.9), (int)(height*.8));
+
+        WindowManager.LayoutParams par = getWindow().getAttributes();
+        par.gravity = Gravity.CENTER;
+        par.x = 0;
+        par.y = 0;
+
+        getWindow().setAttributes(par);
     }
 }
