41f6d7aa1b8493544ca886f4c9b01195b0e4dfbd
riccigamiao3076 <n01363076@humbermail.ca>
2021-11-13 16:15:01 -0500

    Added Rating Bar for Rate App Pop Up Window.

--- SmartPlant/src/main/java/ca/greenlypebble/it/smartplant/RatePopActivity.java
+++ SmartPlant/src/main/java/ca/greenlypebble/it/smartplant/RatePopActivity.java
@@ -1,14 +1,19 @@
 package ca.greenlypebble.it.smartplant;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class RatePopActivity extends Activity {
+
+    RatingBar rateBar;
+    Button submitRate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +34,24 @@ public class RatePopActivity extends Activity {
         par.y = 0;
 
         getWindow().setAttributes(par);
+
+        rateBar = (RatingBar) findViewById(R.id.ratingBar);
+        submitRate = (Button) findViewById(R.id.popRateButton);
+
+        rateBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+
+                ((TextView) findViewById(R.id.tvRate)).setText("Rating: " + rating);
+
+            }
+        });
+
+        submitRate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
